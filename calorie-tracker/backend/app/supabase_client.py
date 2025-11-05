5f4208b1a7a978116a669ad5d178832d475a1537
Noah-Moore <fireballer1000@gmail.com>
2025-11-05 15:13:49 -0600

    Revert "Develop"

--- calorie-tracker/backend/app/supabase_client.py
+++
@@ -1,7 +0,0 @@
-from supabase import create_client, Client
-from decouple import config
-
-SUPABASE_URL = config("SUPABASE_URL")
-SUPABASE_KEY = config("SUPABASE_KEY")
-
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
